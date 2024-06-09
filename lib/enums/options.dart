4f640e988e7d2e083405aea6c332037aac0982e5
Alexander-Guacan <adguacan@espe.edu.ec>
2024-06-09 11:45:35 -0500

    Fix shapes enum names and create option enum

---
+++ lib/enums/options.dart
@@ -0,0 +1,1 @@
+enum Options { a, b, c, d }
--- lib/enums/shapes.dart
+++ lib/enums/shapes.dart
@@ -1,1 +1,10 @@
-enum Shapes { square, triangle, circle, rectangle, Circle1, triangle1, square1, rectangle1 }
+enum Shapes {
+  square,
+  triangle,
+  circle,
+  rectangle,
+  circleHappy,
+  triangleHappy,
+  squareHappy,
+  rectangleHappy
+}
